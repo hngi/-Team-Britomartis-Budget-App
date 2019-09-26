416533f492c69cadf863667668587954b9eec256
Dotun Showunmi <showunmioludotun@gmail.com>
2019-09-26 12:18:31 +0100

    Integrated Registration

--- js/app.js
+++ js/app.js
@@ -1,29 +1,17 @@
 $(() => {
             
     $('#formReg').submit((e) => {
-        alert('clicked');
-
-        // let formData = {
-        //     'name': $('#inputName').val(),
-        //     'phone': $('#inputPhone').val(),
-        //     'email': $('#inputEmail').val(),
-        //     'password': $('#inputPassword').val()
-        // }
-        // $.ajax({
-        //     type: 'POST',
-        //     url: 'api/user',
-        //     data: formData,
-        //     dataType: 'json',
-        //     encode: true
-        // })
-        //     .done((result) => {
-        //         let rep = document.querySelector('#reprt');
-        //         const {data} = result;
-        //         console.log(data);
-        //         rep.classList.add('alert');
-        //         rep.textContent = `Account ${data.email} created successfuly`;
-        //     }) 
         e.preventDefault();
+        let formData =  $('#formReg').serialize();
+        
+        $.ajax({
+            type: 'POST',
+            url: './phpInc/register.php',
+            data: formData,
+            success: function(result) {
+                $('.repot').text(result);
+            }
+        })
     });
 
-}
+});
--- phpInc/register.php
+++ phpInc/register.php
@@ -9,89 +9,88 @@ require 'connect.php';
  
 //If the POST var "register" exists (our submit button), then we can
 //assume that the user has submitted the registration form.
-if(isset($_POST['signupbutton'])){
-    $signup_report = 'Well received'
-
-    // //Retrieve the field values from our registration form.
-    // $firstname = !empty($_POST['firstname']) ? test_input($_POST['firstname']) : null;
-    // // Check if the value of this field is an alphabet and of 3 characters and more
-    // if (!preg_match("/^[a-z]{3,}/i", $firstname)) {
-    //     array_push($errors, "Sorry!! Firstname is too short");
-    // }
-
-    // $lastname = !empty($_POST['lastname']) ? test_input($_POST['lastname']) : null;
-    // // Check if the value of this field is an alphabet and of 3 characters and more
-    // if (!preg_match("/^[a-z]{3,}/i", $firstname)) {
-    //     array_push($errors, "Sorry!! Lastname is too short");
-    // }
-
-    // $email = !empty($_POST['email']) ? test_input($_POST['email']) : null;
-    // // Check if the value of this field is an email
-    // if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //     array_push($errors, "Invalid email format");
-    // }
-
-    // $currency = !empty($_POST['currency']) ? test_input($_POST['currency']) : null;
-
-    // $password1 = !empty($_POST['password']) ? test_input($_POST['password']) : null;
-    // $password2 = !empty($_POST['repeat-password']) ? test_input($_POST['repeat-password']) : null;
-    // //check if the two passwords match
-    // if ($password1 !== $password2){
-    //     array_push($errors, "Password mismatch, please try again.");
-    // }
+if(isset($_POST['reg-email'])){
     
-    // //Now, we need to check if the supplied email already exists.
-    
-    // //Construct the SQL statement and prepare it.
-    // $sql = "SELECT COUNT(email) AS num FROM users WHERE email = :email";
-    // $stmt = $pdo->prepare($sql);
-    
-    // //Bind the provided email to our prepared statement.
-    // $stmt->bindValue(':email', $email);
-    
-    // //Execute.
-    // $stmt->execute();
-    
-    // //Fetch the row.
-    // $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    // //If the provided email already exists - push error.
-    // if($row['num'] > 0){
-    //     array_push($errors, "This email is associated with an existing user!!<br> <a href='#'>Forgot Your Password?</a>");
-    // }
+    //Retrieve the field values from our registration form.
+    $firstname = !empty($_POST['firstname']) ? test_input($_POST['firstname']) : null;
+    // Check if the value of this field is an alphabet and of 3 characters and more
+    if (!preg_match("/[a-z]{3,}/i", $firstname)) {
+        array_push($errors, "Sorry!! Firstname is too short");
+    }
 
-    // // If any error is found in the error array, set signup_report to display_errors function.
-    // if (count($errors) > 0){
-    //     $signup_report = display_errors($errors);
-    //     $last_name = $fullname;
-    //     $last_email = $email;
-    // }
-    // else{
-    //     //Hash the password as we do NOT want to store our passwords in plain text.
-    //     $passwordHash = password_hash($password1, PASSWORD_BCRYPT, array("cost" => 12));
+    $lastname = !empty($_POST['lastname']) ? test_input($_POST['lastname']) : null;
+    // Check if the value of this field is an alphabet and of 3 characters and more
+    if (!preg_match("/[a-z]{3,}/i", $firstname)) {
+        array_push($errors, "Sorry!! Lastname is too short");
+    }
+
+    $email = !empty($_POST['reg-email']) ? test_input($_POST['reg-email']) : null;
+    // Check if the value of this field is an email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, "Invalid email format");
+    }
+
+    $currency = !empty($_POST['currency']) ? test_input($_POST['currency']) : null;
+
+    $password1 = !empty($_POST['reg-password']) ? test_input($_POST['reg-password']) : null;
+    $password2 = !empty($_POST['repeat-password']) ? test_input($_POST['repeat-password']) : null;
+    //check if the two passwords match
+    if ($password1 !== $password2){
+        array_push($errors, "Password mismatch, please try again.");
+    }
+    
+    //Now, we need to check if the supplied email already exists.
+    
+    //Construct the SQL statement and prepare it.
+    $sql = "SELECT COUNT(email) AS num FROM users WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    
+    //Bind the provided email to our prepared statement.
+    $stmt->bindValue(':email', $email);
+    
+    //Execute.
+    $stmt->execute();
+    
+    //Fetch the row.
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    //If the provided email already exists - push error.
+    if($row['num'] > 0){
+        array_push($errors, "This email is associated with an existing user!!");
+    }
+
+    // If any error is found in the error array, set signup_report to display_errors function.
+    if (count($errors) > 0){
+        $signup_report = display_errors($errors);
+    }
+    else{
+        //Hash the password as we do NOT want to store our passwords in plain text.
+        $passwordHash = password_hash($password1, PASSWORD_BCRYPT, array("cost" => 12));
         
-    //     //Prepare our INSERT statement.
-    //     //Remember: We are inserting a new row into our users table.
-    //     $sql = "INSERT INTO users (firstname, lastname, email, currency, userpass, reg_date) VALUES (:firstname, :lastname, :email, :currency, :password, NOW())";
-    //     $stmt = $pdo->prepare($sql);
+        //Prepare our INSERT statement.
+        //Remember: We are inserting a new row into our users table.
+        $sql = "INSERT INTO users (firstname, lastname, email, currency, userpass, reg_date) VALUES (:firstname, :lastname, :email, :currency, :password, NOW())";
+        $stmt = $pdo->prepare($sql);
         
-    //     //Bind our variables.
-    //     $stmt->bindValue(':firstname', $firstname);
-    //     $stmt->bindValue(':lastname', $lastname);
-    //     $stmt->bindValue(':email', $email);
-    //     $stmt->bindValue(':currency', $currency);
-    //     $stmt->bindValue(':password', $passwordHash);
+        //Bind our variables.
+        $stmt->bindValue(':firstname', $firstname);
+        $stmt->bindValue(':lastname', $lastname);
+        $stmt->bindValue(':email', $email);
+        $stmt->bindValue(':currency', $currency);
+        $stmt->bindValue(':password', $passwordHash);
     
-    //     //Execute the statement and insert the new account.
-    //     $result = $stmt->execute();
+        //Execute the statement and insert the new account.
+        $result = $stmt->execute();
         
-    //     //If the signup process is successful.
-    //     if($result){
-    //         //set signup report to success
-    //         $signup_report = 'Your Sign up was Successful. Please <a href="index.php">login</a>';
-    //     }
+        //If the signup process is successful.
+        if($result){
+            //set signup report to success
+            $signup_report = 'Success! Your Sign up was Successful.';
+        }
 
-    // }
+    }
+
+    echo $signup_report;
     
 }
  
