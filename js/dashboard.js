171675755b6cc8a47920420aaa5a9e3bb11db49a
Dotun Showunmi <showunmioludotun@gmail.com>
2019-10-05 08:36:23 +0100

    Edit amount and distribute the rest fixed by @Tombra

--- js/dashboard.js
+++ js/dashboard.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     allItems = ` <tr id="itemObj">
     <td class="text-capitalize"><b id="itemRow">${item}</b></td>
     <td class="text-capitalize"><b id="categoryRow">${category}</b></td>
-    <td class="text-capitalize"><input onkeyup="testInput(this, this.value)" id="amount" type="number" value="0" min="0" size="4" /></td>
+    <td class="text-capitalize"><input onkeyup="testInput(this, this.value)" id="amount" type="text" value="0" min="0" size="4" /></td>
       <td class="text-capitalize"><b id="priorityRow">${priority}</b></td>
       <td>
         <button class="btn btn-danger" id ="deleteBtn" name="deleteBtn" onclick="deleted()">
@@ -56,8 +56,10 @@ function deleted() {
 function calculate() {
   // let table = document.getElementById("tableid");
   // let tableRows = table.rows.length;
+  let budgetedAmount = document.querySelector('#budgetedamount').value;
+
   const items = document.querySelectorAll('#itemObj');
-  if (items.length == 0 || budgetedamount == "") {
+  if (items.length == 0 || budgetedAmount == "") {
     alert = `<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
     <strong>Error!</strong>  Items Rows or Enter Total Available Sum cannot be empty.
     <button
@@ -111,6 +113,45 @@ function calculate() {
     
     let amount;
 
+    //get the input fields that were changed
+    for (let i = 0; i < prioArr.length; i++) {
+      if(globalAmountArray[i] !== Number.parseFloat(amountRow[i].value)){
+        golbalTestArray[i] = true;
+      }
+    }
+
+    //get the sum of the priorities needed and the total fixed amount
+    let sum = 0;
+    let totalFixedAmount = 0;
+    for (let i = 0; i < prioArr.length; i++) {
+      if(golbalTestArray[i]){
+        totalFixedAmount += Number.parseFloat(amountRow[i].value);
+        continue;
+      }
+      sum += prioArr[i];
+    }
+
+    if(sumAmount() <= budgetedAmount.value){
+      //insert the values to their designated input fields
+      for (let i = 0; i < prioArr.length; i++) {
+        //check if the value does not need to be changed
+        if(golbalTestArray[i]){
+          //update the previous amount array
+          globalAmountArray[i] = Number.parseFloat(amountRow[i].value);
+          //update the test array back to false
+          golbalTestArray[i] = false;
+          continue;
+        }
+        amount = (prioArr[i] / sum) * (budgetedAmount.value - totalFixedAmount); 
+        amountRow[i].value = Math.round(amount * 100) / 100;
+
+        //update the previous amount array
+        globalAmountArray[i] = Number.parseFloat(amountRow[i].value);
+        //update the test array back to false
+        golbalTestArray[i] = false;
+      }
+      return;
+    }
     if(sumAmount() == 0){
       for (let i = 0; i < prioArr.length; i++) {
         amount = (prioArr[i] / prioritySum) * budgetedAmount.value
@@ -118,63 +159,55 @@ function calculate() {
         amountRow[i].value = Math.round(amount * 100) / 100;
         globalAmountArray[i] = Number.parseFloat(amountRow[i].value);
       }
-    }else if(sumAmount() < budgetedAmount.value){
-      //get the input fields that were changed
-      for (let i = 0; i < prioArr.length; i++) {
-        if(globalAmountArray[i] !== Number.parseFloat(amountRow[i].value)){
-          golbalTestArray[i] = true;
-        }
-      }
-
-      //get the sum of the priorities needed and the total fixed amount
-      let sum = 0;
-      let totalFixedAmount = 0;
-      for (let i = 0; i < prioArr.length; i++) {
-        if(golbalTestArray[i]){
-          totalFixedAmount += Number.parseFloat(amountRow[i].value);
-          continue;
-        }
-        sum += prioArr[i];
-      }
-
-      //insert the values to their designated input fields
-      for (let i = 0; i < prioArr.length; i++) {
-        //update the previous amount array
-        globalAmountArray[i] = Number.parseFloat(amountRow[i].value);
-        //update the test array back to false
-        golbalTestArray[i] = false;
-
-        //check if the value does not need to be changed
-        if(golbalTestArray[i]){
-          continue;
-        }
-        amount = (prioArr[i] / sum) * (budgetedAmount.value - totalFixedAmount); 
-        amountRow[i].value = Math.round(amount * 100) / 100;
-      }
     }else{
-      for (let i = 0; i < prioArr.length; i++) {
-        amountRow[i].value = 0;
-        globalAmountArray[i] = +Number.parseFloat(amountRow[i].value);
+      if(totalFixedAmount > budgetedAmount.value){
+        for (let i = 0; i < prioArr.length; i++) {
+          amountRow[i].value = 0;
+          //update the previous amount array
+          globalAmountArray[i] = Number.parseFloat(amountRow[i].value);
+          //update the test array back to false
+          golbalTestArray[i] = false;
+        }
+  
+        let alert = `<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+                      <strong>Error!</strong> Entered Item(s) greater than Budgeted Amount!
+                      <button
+                        type="button"
+                        class="close"
+                        data-dismiss="alert"
+                        aria-label="Close"
+                      >
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>`;
+    
+        $('#myAlert').html(alert);
+    
+        setTimeout(() => {
+          $('.alert').remove();
+        }, 3000);
+
+        return;
+      }else{
+        //insert the values to their designated input fields
+        for (let i = 0; i < prioArr.length; i++) {
+          //check if the value does not need to be changed
+          if(golbalTestArray[i]){
+            //update the previous amount array
+            globalAmountArray[i] = Number.parseFloat(amountRow[i].value);
+            //update the test array back to false
+            golbalTestArray[i] = false;
+            continue;
+          }
+          amount = (prioArr[i] / sum) * (budgetedAmount.value - totalFixedAmount); 
+          amountRow[i].value = Math.round(amount * 100) / 100;
+
+          //update the previous amount array
+          globalAmountArray[i] = Number.parseFloat(amountRow[i].value);
+          //update the test array back to false
+          golbalTestArray[i] = false;
+        }
       }
-
-      let alert = `<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
-                    <strong>Error!</strong>  Items amount greater than budgeted amount!
-                    <button
-                      type="button"
-                      class="close"
-                      data-dismiss="alert"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>`;
-
-    $('#myAlert').html(alert);
-
-    setTimeout(() => {
-      $('.alert').remove();
-    }, 3000);
-
     }
   }
 }
@@ -185,7 +218,7 @@ function sumAmount(){
   let sum = 0;
 
   for (let i = 0; i < amountRow.length; i++) {
-    let item = Number.parseFloat(amountRow[i].value);
+    let item = Number.parseFloat(amountRow[i].value) || 0;
     sum += item;
   }
 
@@ -195,7 +228,14 @@ function sumAmount(){
 //This function is used to edit the user input as necessary
 function testInput(element, value){
   if (Number.isNaN(Number.parseFloat(value))) {
-    element.value = 0;
-    return;
+    if(value == ""){
+      element.value = 0;
+      return;
+    }
+      element.value.charAt(value.length - 1) = "";
+      return;
+  }else{
+    element.value = Number.parseFloat(value);
   }
+
 }
